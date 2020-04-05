17f5913c93e3744a21df5ee8ac1263532dea0705
Diogo Trindade <diogotr7@gmail.com>
2020-04-05 17:06:59 +0100

    Added steelseries project template

---
+++ Project-Aurora/Devices/Device-Steelseries/SteelseriesDevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aurora.Devices;
+using System.Drawing;
+
+namespace Device_Steelseries
+{
+    public class SteelseriesDevice : Device
+    {
+        protected override string DeviceName => "Steelseries";
+
+        public override bool Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UpdateDevice(Dictionary<DeviceKeys, System.Drawing.Color> keyColors, DoWorkEventArgs e, bool forced = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
