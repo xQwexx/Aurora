68a2008c5fc9bf70729b27c432fbe8da717fd682
WildWolf <magnus1997.mho@gmail.com>
2020-04-08 14:53:52 +0200

    Fully implement SteelSeries support in DLL format

---
+++ Project-Aurora/Devices/Device-Steelseries/SteelSeriesDevice.GameSense.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Aurora;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Device_SteelSeries
+{
+    public partial class SteelSeriesDevice
+    {
+        private HttpClient client;
+        private JObject baseObject = new JObject();
+        private JObject baseColorObject = new JObject {{"Event", "AURORA"}, {"data", new JObject()}};
+        private Task pingTask;
+        private CancellationTokenSource pingTaskTokenSource = new CancellationTokenSource();
+        private bool loadedLisp;
+        private JObject dataColorObject => (JObject)baseColorObject["data"];
+
+        private void sendLispCode()
+        {
+            try
+            {
+                var core = (JObject)baseObject.DeepClone();
+                core.Add("game_display_name", "Project Aurora");
+                core.Add("icon_color_id", 0);
+                sendJson("/game_metadata", core);
+                core.Remove("game_display_name");
+                core.Remove("icon_color_id");
+                using (StreamReader reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Device_SteelSeries.GoCode.lsp")))
+                {
+                    core.Add("golisp", reader.ReadToEnd());
+                }
+                sendJson("/load_golisp_handlers", core);
+                pingTask = Task.Run(async () => await sendPing(pingTaskTokenSource.Token), pingTaskTokenSource.Token);
+                loadedLisp = true;
+            }
+            catch (Exception e)
+            {
+                Global.logger.Error(e,"SteelSeries Lisp Code failed.");
+                throw;
+            }
+        }
+
+        private void loadCoreProps()
+        {
+            var file = new FileInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "SteelSeries/SteelSeries Engine 3/coreProps.json"));
+            if (!file.Exists)
+                throw new FileNotFoundException($"Core Props file could not be found.");
+
+            try
+            {
+                var reader = file.OpenText();
+                var coreProps = JObject.Parse(reader.ReadToEnd());
+                reader.Dispose();
+                client.BaseAddress = new Uri("http://" + coreProps["address"]);
+                sendLispCode();
+            }
+            catch (Exception e)
+            {
+                Global.logger.Error(e, "SteelSeries Core Props Load failed.");
+            }
+        }
+
+        private void setKeyboardLed(byte led, Color color)
+        {
+            if(!(dataColorObject).ContainsKey("keyboard"))
+                dataColorObject.Add("keyboard", new JObject {{"hids", new JArray()}, {"colors", new JArray()}});
+            ((JArray) dataColorObject["keyboard"]["hids"]).Add(led);
+            ((JArray) dataColorObject["keyboard"]["colors"]).Add(new JArray{color.R, color.G, color.B});
+        }
+
+        private void setOneZone(Color color)
+        {
+            dataColorObject.Add("onezone", new JObject{{"color",new JArray {color.R, color.G, color.B}}});
+        }
+
+        private void setTwoZone(Color[] colors)
+        {
+            dataColorObject.Add("twozone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setThreeZone(Color[] colors)
+        {
+            dataColorObject.Add("threezone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setFourZone(Color[] colors)
+        {
+            dataColorObject.Add("fourzone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setFiveZone(Color[] colors)
+        {
+            dataColorObject.Add("fivezone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setEightZone(Color[] colors)
+        {
+            dataColorObject.Add("eightzone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setTwelveZone(Color[] colors)
+        {
+            dataColorObject.Add("twelvezone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setSeventeenZone(Color[] colors)
+        {
+            dataColorObject.Add("seventeenzone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setTwentyFourZone(Color[] colors)
+        {
+            dataColorObject.Add("twentyfourzone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setHundredThreeZone(Color[] colors)
+        {
+            dataColorObject.Add("hundredthreezone", new JObject{{"colors", colorToJson(colors)}});
+        }
+
+        private void setLogo(Color color)
+        {
+            dataColorObject.Add("logo", new JObject{{"color",new JArray {color.R, color.G, color.B}}});
+        }
+
+        private void setWheel(Color color)
+        {
+            dataColorObject.Add("wheel", new JObject{{"color",new JArray {color.R, color.G, color.B}}});
+        }
+
+        private void setMouse(Color color)
+        {
+            dataColorObject.Add("mouse", new JObject{{"color",new JArray {color.R, color.G, color.B}}});
+        }
+
+        private void setGeneric(Color color)
+        {
+            dataColorObject.Add("periph", new JObject{{"color",new JArray {color.R, color.G, color.B}}});
+        }
+
+        private async Task sendPing(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(10));
+                    await sendJsonAsync("/game_heartbeat", baseObject);
+                }
+                catch (Exception e)
+                {
+                    Global.logger.Error(e, "Error while sending heartbeat to SteelSeries Engine trying to restart.");
+                    //To stop all other events from erroring and try and reboot steelseries engine
+                    Reset();
+                }
+            }
+        }
+
+        private void sendLighting()
+        {
+            sendJson("/game_event", baseColorObject);
+        }
+
+        private void sendJson(string endpoint, object obj)
+        {
+            var t = sendJsonAsync(endpoint, obj).GetAwaiter().GetResult();
+        }
+
+        private Task<HttpResponseMessage> sendJsonAsync(string endpoint, object obj)
+        {
+            return client.PostAsync(endpoint, new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json"));
+        }
+
+        private static JArray colorToJson(Color[] colors)
+        {
+            return new JArray(colors.Select(color => new JArray {color.R, color.G, color.B}).ToArray());
+        }
+    }
+}
--- Project-Aurora/Devices/Device-Steelseries/SteelseriesDevice.cs
+++ Project-Aurora/Devices/Device-Steelseries/SteelseriesDevice.cs
@@ -1,31 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Aurora.Devices;
+using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Aurora;
+using Aurora.Devices;
+using Aurora.Devices.SteelSeries;
+using Aurora.Settings;
+using Aurora.Utils;
+using Newtonsoft.Json.Linq;
 
-namespace Device_Steelseries
+namespace Device_SteelSeries
 {
-    public class SteelseriesDevice : Device
+    public partial class SteelSeriesDevice : Device
     {
-        protected override string DeviceName => "Steelseries";
+        TimeSpan updateTime = TimeSpan.Zero;
+        object lock_obj = new object();
+
+        public override VariableRegistry GetRegisteredVariables()
+        {
+            return new VariableRegistry();
+        }
+
+        protected override string DeviceName => "SteelSeries";
+
+        public override string GetDeviceDetails() => IsInitialized() ? DeviceName + ": Connected" : DeviceName + ": Not initialized";
+
+        public override string GetDeviceUpdatePerformance()
+        {
+            return (IsInitialized() ? getDeviceUpdateTime() + " ms" : "");
+        }
+
+        private string getDeviceUpdateTime()
+        {
+            return updateTime.TotalMilliseconds > 1000 ? "Restart SteelSeries Engine it has not responded for over 1000" : ((int)updateTime.TotalMilliseconds).ToString();
+        }
 
         public override bool Initialize()
         {
-            throw new NotImplementedException();
+            lock (lock_obj)
+            {
+                try
+                {
+                    if (!baseObject.ContainsKey("game"))
+                    {
+                        baseObject.Add("game", "PROJECTAURORA");
+                        baseColorObject.AddFirst(new JProperty("game", baseObject["game"]));
+                    }
+                    client = new HttpClient {Timeout = TimeSpan.FromSeconds(30)};
+                    loadCoreProps();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Global.logger.Error("SteelSeries SDK could not be initialized: " + e);
+                    return false;
+                }
+            }
         }
 
         public override void Shutdown()
         {
-            throw new NotImplementedException();
+            lock (lock_obj)
+            {
+                pingTaskTokenSource.Cancel();
+                client?.Dispose();
+                loadedLisp = false;
+            }
         }
 
-        public override bool UpdateDevice(Dictionary<DeviceKeys, System.Drawing.Color> keyColors, DoWorkEventArgs e, bool forced = false)
+        public override void Reset()
         {
-            throw new NotImplementedException();
+            Shutdown();
+            lock (lock_obj)
+            {
+                Process.Start(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\SteelSeries\SteelSeries Engine 3\SteelSeries Engine 3.lnk");
+                Task.Delay(15000).GetAwaiter().GetResult();
+            }
+            Initialize();
+        }
+
+        public override bool Reconnect() => true;
+
+        public override bool IsInitialized() => loadedLisp;
+
+        public override bool IsConnected() => loadedLisp;
+
+        public override bool IsKeyboardConnected() => IsConnected();
+
+        public override bool IsPeripheralConnected() => IsConnected();
+
+        public override bool UpdateDevice(Dictionary<DeviceKeys, Color> keyColors, DoWorkEventArgs e, bool forced = false)
+        {
+            foreach (var (key, color) in keyColors)
+            {
+                if (TryGetHid(key, out var hid))
+                {
+                    setKeyboardLed(hid, color);
+                }
+            }
+            return true;
+        }
+
+        public override bool UpdateDevice(DeviceColorComposition colorComposition, DoWorkEventArgs e, bool forced = false)
+        {
+            var tmpTime = DateTime.Now;
+            var keyColors = colorComposition.keyColors.ToDictionary(t => t.Key, t => ColorUtils.MultiplyColorByScalar(t.Value, t.Value.A / 255D));
+            dataColorObject.RemoveAll();
+            if (!Global.Configuration.devices_disable_mouse || !Global.Configuration.devices_disable_headset)
+            {
+                if (!keyColors.ContainsKey(DeviceKeys.Peripheral))
+                {
+                    var mousePad = keyColors.Where(t => t.Key >= DeviceKeys.SSMPL1 && t.Key <= DeviceKeys.SSMPL12).Select(t => t.Value).ToArray();
+                    var mouse = new List<Color> { keyColors[DeviceKeys.Peripheral_Logo], keyColors[DeviceKeys.Peripheral_ScrollWheel]};
+                    mouse.AddRange(keyColors.Where(t => t.Key <= DeviceKeys.MOUSELIGHT1 && t.Key >= DeviceKeys.MOUSELIGHT6).Select(t => t.Value));
+                    setOneZone(keyColors[DeviceKeys.Peripheral_Logo]);
+                    if (mouse.Count <= 1)
+                        setMouse(keyColors[DeviceKeys.Peripheral_Logo]);
+                    else
+                    {
+                        setLogo(keyColors[DeviceKeys.Peripheral_Logo]);
+                        setWheel(keyColors[DeviceKeys.Peripheral_ScrollWheel]);
+                        if (mouse.Count == 8)
+                            setEightZone(mouse.ToArray());
+                    }
+                    if (mousePad.Length == 2)
+                        setTwoZone(mousePad);
+                    else
+                        setTwelveZone(mousePad);
+                    if (keyColors.Count(t => t.Key >= DeviceKeys.MONITORLIGHT1 && t.Key <= DeviceKeys.MONITORLIGHT103) == 103)
+                        setHundredThreeZone(keyColors.Where(t => t.Key >= DeviceKeys.MONITORLIGHT1 && t.Key <= DeviceKeys.MONITORLIGHT103).Select(t => t.Value).ToArray());
+                }
+                else
+                    setGeneric(keyColors[DeviceKeys.Peripheral]);
+            }
+            if (!Global.Configuration.devices_disable_keyboard)
+            {
+                UpdateDevice(keyColors, e, forced);
+            }
+            sendLighting();
+            updateTime = DateTime.Now - tmpTime;
+            return true;
         }
     }
 }
