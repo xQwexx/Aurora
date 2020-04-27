28b86b3bf44f894127401ceafe521b4a04ab133a
gitmacer <timfirefox@gmail.com>
2020-04-28 00:58:50 +0200

    (Questions are left) Update Roccat implementation to get it to work
    (Still big Questions)

--- Project-Aurora/Devices/Device-Roccat/RoccatDevice.cs
+++ Project-Aurora/Devices/Device-Roccat/RoccatDevice.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,34 +11,47 @@ using Roccat_Talk.TalkFX;
 using Roccat_Talk.RyosTalkFX;
 using Aurora.Utils;
 using Color = System.Drawing.Color;
+using Aurora;
+using Aurora.Settings;
 
 namespace Device_Roccat
 {
     public class RoccatDevice : Device
     {
-        protected override string DeviceName => "Roccat";
+        protected override string DeviceName => "Roccat Talk";
+        private DeviceKeys device_key;
+        private bool enable_generic;
+        private bool enable_ryos;
         private TalkFxConnection talkFx;
         private RyosTalkFXConnection ryosTalkFx;
         private readonly byte[] stateStruct = new byte[110];
         private readonly Roccat_Talk.TalkFX.Color[] colorStruct = new Roccat_Talk.TalkFX.Color[110];
 
-
         public override bool Initialize()
         {
             if (isInitialized)
                 return true;
-            
+
+            device_key = GlobalVarRegistry.GetVariable<DeviceKeys>($"{DeviceName}_devicekey");
+            enable_generic = GlobalVarRegistry.GetVariable<bool>($"{DeviceName}_enable_generic");
+            enable_ryos = GlobalVarRegistry.GetVariable<bool>($"{DeviceName}_enable_ryos");
+
             talkFx = new TalkFxConnection();
             ryosTalkFx = new RyosTalkFXConnection();
 
+            //Will return true even when no Ryos is connected. Check if TalkFx is opened?
+            //Is even requierd for 1 color devices with the used sdk version.
             if (!ryosTalkFx.Initialize())
             {
-                LogError("Could not initialize Ryos TalkFX");
+                LogInfo("Could not initialize Ryos Talk Fx");
                 return isInitialized = false;
             }
 
+            //Will return true even when no Ryos is connected. Check if TalkFx is opened?
+            //Is even requierd for 1 color devices with the used sdk version.
             if (!ryosTalkFx.EnterSdkMode())
             {
+                LogError("Could not Enter Ryos SDK Mode");
                 return isInitialized = false;
             }
 
@@ -47,11 +60,16 @@ namespace Device_Roccat
 
         public override void Shutdown()
         {
-            var restore = new Roccat_Talk.TalkFX.Color(0, 0, 0);//TODO: get restore color 
+            //Shutdown 1 color devices.
+            var restoreColor = ToRoccatColor(GlobalVarRegistry.GetVariable<RealColor>($"{DeviceName}_restore_fallback"));
+            talkFx?.SetLedRgb(Zone.Event, KeyEffect.On, Speed.Fast, restoreColor); //Workaround because "RestoreLedRgb()" doesn't seam to work.
+            talkFx?.RestoreLedRgb();
+            talkFx?.Dispose();
+
+            //Shutdown per key keyboards.
             ryosTalkFx?.ExitSdkMode();
             ryosTalkFx?.Dispose();
-            talkFx?.SetLedRgb(Zone.Event, KeyEffect.On, Speed.Fast, restore);
-            talkFx?.Dispose();
+
             isInitialized = false;
         }
 
@@ -60,21 +78,39 @@ namespace Device_Roccat
             if (!isInitialized)
                 return false;
 
-            foreach(var key in keyColors)
+            if (enable_ryos)
             {
-                if (KeyMap.DeviceKeysMap.TryGetValue(key.Key, out var k))
+                foreach (var key in keyColors)
                 {
-                    colorStruct[k] = ToRoccatColor(key.Value);
-                    stateStruct[k] = IsLedOn(key.Value);
+                    if (KeyMap.DeviceKeysMap.TryGetValue(LocalizeLayout(key.Key), out var k))
+                    {
+                        colorStruct[k] = ToRoccatColor(key.Value);
+                        stateStruct[k] = IsLedOn(key.Value);
+                    }
                 }
+                ryosTalkFx.SetMkFxKeyboardState(stateStruct, colorStruct, (byte)GetLayout());
             }
-            ryosTalkFx.SetMkFxKeyboardState(stateStruct, colorStruct, 0x01);
+
+            //Set 1 color devices
+            if (enable_generic && keyColors.TryGetValue(device_key, out var clr))
+            {
+                talkFx.SetLedRgb(Zone.Event, KeyEffect.On, Speed.Fast, ToRoccatColor(clr));
+            }
 
             return true;
         }
 
+        protected override void RegisterVariables(VariableRegistry local)
+        {
+            local.Register($"{DeviceName}_devicekey", DeviceKeys.Peripheral_Logo, "Key to Use", DeviceKeys.MOUSEPADLIGHT15, DeviceKeys.Peripheral_Logo);
+            local.Register($"{DeviceName}_restore_fallback", new RealColor(System.Drawing.Color.FromArgb(255, 0, 0, 255)), "Color", new Aurora.Utils.RealColor(System.Drawing.Color.FromArgb(255, 255, 255, 255)), new Aurora.Utils.RealColor(System.Drawing.Color.FromArgb(0, 0, 0, 0)), "Set restore color for your generic roccat devices");
+            local.Register($"{DeviceName}_enable_generic", true, "Enable 1 color devices");
+            local.Register($"{DeviceName}_enable_ryos", true, "Enable per key devices");
+        }
+
         private static Roccat_Talk.TalkFX.Color ToRoccatColor(Color c) =>
             new Roccat_Talk.TalkFX.Color(c.R, c.G, c.B);
+
         private byte IsLedOn(Color roccatColor)
         {
             if (roccatColor.R == 0 && roccatColor.G == 0 && roccatColor.B == 0)
@@ -83,5 +119,154 @@ namespace Device_Roccat
             }
             return 1;
         }
+
+        private DeviceLayout GetLayout()
+        {
+            if (Global.Configuration.keyboard_localization == PreferredKeyboardLocalization.dvorak
+                || Global.Configuration.keyboard_localization == PreferredKeyboardLocalization.us
+                || Global.Configuration.keyboard_localization == PreferredKeyboardLocalization.ru)
+                return DeviceLayout.ANSI;
+            else if (Global.Configuration.keyboard_localization == PreferredKeyboardLocalization.jpn)
+                return DeviceLayout.JP;
+            else
+                return DeviceLayout.ISO;
+        }
+
+        private DeviceKeys LocalizeLayout(DeviceKeys Key)
+        {
+            //Solution to slightly different mapping rather than giving a whole different dictionary
+
+            //Unclean sulution should be changed!
+            if (GetLayout() == DeviceLayout.ISO) //Was ANSI but I am pretty sure that what a typo?
+            {
+                if (Key == DeviceKeys.ENTER)
+                    Key = DeviceKeys.BACKSLASH;
+                if (Key == DeviceKeys.HASHTAG)
+                    Key = DeviceKeys.ENTER;
+            }
+            return Key;
+        }
+
+        public enum DeviceLayout : byte
+        {
+            ISO = 0,
+            ANSI = 1,
+            JP = 2
+        }
+
+        //Ansi Layout? SDK Docs are mentioning Enter = 52 is DE/iso Layout
+        public static Dictionary<DeviceKeys, byte> DeviceKeysMap = new Dictionary<DeviceKeys, byte>
+        {
+            {DeviceKeys.ESC, 0 },
+            {DeviceKeys.F1, 1 },
+            {DeviceKeys.F2, 2 },
+            {DeviceKeys.F3, 3 },
+            {DeviceKeys.F4, 4 },
+            {DeviceKeys.F5, 5 },
+            {DeviceKeys.F6, 6 },
+            {DeviceKeys.F7, 7 },
+            {DeviceKeys.F8, 8 },
+            {DeviceKeys.F9, 9 },
+            {DeviceKeys.F10, 10 },
+            {DeviceKeys.F11, 11 },
+            {DeviceKeys.F12, 12 },
+            {DeviceKeys.PRINT_SCREEN, 13 },
+            {DeviceKeys.SCROLL_LOCK, 14 },
+            {DeviceKeys.PAUSE_BREAK, 15 },
+            {DeviceKeys.G1, 16 },
+            {DeviceKeys.TILDE, 17 },
+            {DeviceKeys.ONE, 18 },
+            {DeviceKeys.TWO, 19 },
+            {DeviceKeys.THREE, 20 },
+            {DeviceKeys.FOUR, 21 },
+            {DeviceKeys.FIVE, 22 },
+            {DeviceKeys.SIX, 23 },
+            {DeviceKeys.SEVEN, 24 },
+            {DeviceKeys.EIGHT, 25 },
+            {DeviceKeys.NINE, 26 },
+            {DeviceKeys.ZERO, 27 },
+            {DeviceKeys.MINUS, 28 },
+            {DeviceKeys.EQUALS, 29 },
+            {DeviceKeys.BACKSPACE, 30 },
+            {DeviceKeys.INSERT, 31 },
+            {DeviceKeys.HOME, 32 },
+            {DeviceKeys.PAGE_UP, 33 },
+            {DeviceKeys.NUM_LOCK, 34 },
+            {DeviceKeys.NUM_SLASH, 35 },
+            {DeviceKeys.NUM_ASTERISK, 36 },
+            {DeviceKeys.NUM_MINUS, 37 },
+            {DeviceKeys.G2, 38 },
+            {DeviceKeys.TAB, 39 },
+            {DeviceKeys.Q, 40 },
+            {DeviceKeys.W, 41 },
+            {DeviceKeys.E, 42 },
+            {DeviceKeys.R, 43 },
+            {DeviceKeys.T, 44 },
+            {DeviceKeys.Y, 45 },
+            {DeviceKeys.U, 46 },
+            {DeviceKeys.I, 47 },
+            {DeviceKeys.O, 48 },
+            {DeviceKeys.P, 49 },
+            {DeviceKeys.OPEN_BRACKET, 50 },
+            {DeviceKeys.CLOSE_BRACKET, 51 },
+            {DeviceKeys.BACKSLASH, 52 },
+            {DeviceKeys.DELETE, 53 },
+            {DeviceKeys.END, 54 },
+            {DeviceKeys.PAGE_DOWN, 55 },
+            {DeviceKeys.NUM_SEVEN, 56 },
+            {DeviceKeys.NUM_EIGHT, 57 },
+            {DeviceKeys.NUM_NINE, 58 },
+            {DeviceKeys.NUM_PLUS, 59 },
+            {DeviceKeys.G3, 60 },
+            {DeviceKeys.CAPS_LOCK, 61 },
+            {DeviceKeys.A, 62 },
+            {DeviceKeys.S, 63 },
+            {DeviceKeys.D, 64 },
+            {DeviceKeys.F, 65 },
+            {DeviceKeys.G, 66 },
+            {DeviceKeys.H, 67 },
+            {DeviceKeys.J, 68 },
+            {DeviceKeys.K, 69 },
+            {DeviceKeys.L, 70 },
+            {DeviceKeys.SEMICOLON, 71 },
+            {DeviceKeys.APOSTROPHE, 72 },
+            {DeviceKeys.ENTER, 73 },
+            {DeviceKeys.NUM_FOUR, 74 },
+            {DeviceKeys.NUM_FIVE, 75 },
+            {DeviceKeys.NUM_SIX, 76 },
+            {DeviceKeys.G4, 77 },
+            {DeviceKeys.LEFT_SHIFT, 78 },
+            {DeviceKeys.BACKSLASH_UK, 79 },
+            {DeviceKeys.Z, 80 },
+            {DeviceKeys.X, 81 },
+            {DeviceKeys.C, 82 },
+            {DeviceKeys.V, 83 },
+            {DeviceKeys.B, 84 },
+            {DeviceKeys.N, 85 },
+            {DeviceKeys.M, 86 },
+            {DeviceKeys.COMMA, 87 },
+            {DeviceKeys.PERIOD, 88 },
+            {DeviceKeys.FORWARD_SLASH, 89 },
+            {DeviceKeys.RIGHT_SHIFT, 90 },
+            {DeviceKeys.ARROW_UP, 91 },
+            {DeviceKeys.NUM_ONE, 92 },
+            {DeviceKeys.NUM_TWO, 93 },
+            {DeviceKeys.NUM_THREE, 94 },
+            {DeviceKeys.NUM_ENTER, 95 },
+            {DeviceKeys.G5, 96 },
+            {DeviceKeys.LEFT_CONTROL, 97 },
+            {DeviceKeys.LEFT_WINDOWS, 98 },
+            {DeviceKeys.LEFT_ALT, 99 },
+            {DeviceKeys.SPACE, 100 },
+            {DeviceKeys.RIGHT_ALT, 101 },
+            {DeviceKeys.FN_Key, 102 },
+            {DeviceKeys.APPLICATION_SELECT, 103 },
+            {DeviceKeys.RIGHT_CONTROL, 104 },
+            {DeviceKeys.ARROW_LEFT, 105 },
+            {DeviceKeys.ARROW_DOWN, 106 },
+            {DeviceKeys.ARROW_RIGHT, 107 },
+            {DeviceKeys.NUM_ZERO, 108 },
+            {DeviceKeys.NUM_PERIOD, 109 }
+        };
     }
 }
